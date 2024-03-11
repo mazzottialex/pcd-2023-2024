9f46e585ea5b832210ddb197d1f3989080208227
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-03-11 13:49:56 +0100

    lab 2

--- app/src/main/java/pcd/lab02/lost_updates/Worker.java
+++ app/src/main/java/pcd/lab02/lost_updates/Worker.java
@@ -5,17 +5,21 @@ public class Worker extends Thread {
 	
 	private UnsafeCounter counter;
 	private int ntimes;
-	
-	public Worker(String name, UnsafeCounter counter, int ntimes){
+	private Object lock;
+
+	public Worker(String name, UnsafeCounter counter, int ntimes, Object lock){
 		super(name);
 		this.counter = counter;
 		this.ntimes = ntimes;
+		this.lock = lock;
 	}
 	
 	public void run(){
 		log("started");
 		for (int i = 0; i < ntimes; i++){
-			counter.inc();
+			synchronized(lock){
+				counter.inc();
+			}
 		}
 		log("completed");
 	}
